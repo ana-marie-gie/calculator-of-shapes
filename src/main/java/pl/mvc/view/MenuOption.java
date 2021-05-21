cd42655ca0d8e75f71aa0096949ddf5f4906791c
ania <anagie@icloud.com>
2021-05-21 18:43:22 +0200

    added in enum MenuOption menu indexes: EXIT, CIRCLE_FIELD

--- src/main/java/pl/mvc/view/MenuOption.java
+++ src/main/java/pl/mvc/view/MenuOption.java
@@ -1,4 +1,21 @@
 package pl.mvc.view;
 
 public enum MenuOption {
+    EXIT(0),
+    CIRCLE_FIELD(1);
+
+    public int menuIndex;
+
+    MenuOption(int menuIndex) {
+        this.menuIndex = menuIndex;
+    }
+
+    public static MenuOption valueOf(int menuIndex) {
+        for (MenuOption menuOption : values()) {
+            if (menuIndex == menuOption.menuIndex) {
+                return menuOption;
+            }
+        }
+        return null;
+    }
 }
