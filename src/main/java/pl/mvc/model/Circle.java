c05d9a90f66c82a780499ee539353be15b2e98f4
ania <anagie@icloud.com>
2021-05-21 22:49:43 +0200

    removed in class model Circle lombok @NoArgsConstructor

--- src/main/java/pl/mvc/model/Circle.java
+++ src/main/java/pl/mvc/model/Circle.java
@@ -2,10 +2,8 @@ package pl.mvc.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
-@NoArgsConstructor
 @Data
 public class Circle {
     private double r;
