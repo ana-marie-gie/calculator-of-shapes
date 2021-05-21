eb0f2df5ac804a2134c149bfd04be03c3fb7c78a
ania <anagie@icloud.com>
2021-05-21 18:41:32 +0200

    added to interface View methods: MenuOption showMenuAndGetOption(), Circle promptForCircleParameters(), void showCircleField(double circleField);

--- src/main/java/pl/mvc/view/View.java
+++ src/main/java/pl/mvc/view/View.java
@@ -1,4 +1,11 @@
 package pl.mvc.view;
 
+import pl.mvc.model.Circle;
+
 public interface View {
+    MenuOption showMenuAndGetOption();
+
+    Circle promptForCircleParameters();
+
+    void showCircleField(double circleField);
 }
