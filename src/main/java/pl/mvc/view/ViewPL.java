f1a35d0b87442883227bff0facc877d6556f4eef
ania <anagie@icloud.com>
2021-05-21 22:43:22 +0200

    added menu option: change_language  and modify method: handleLanguageChange()

--- src/main/java/pl/mvc/view/ViewPL.java
+++ src/main/java/pl/mvc/view/ViewPL.java
@@ -2,29 +2,29 @@ package pl.mvc.view;
 
 import pl.mvc.model.Circle;
 
-import java.awt.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class ViewPL implements View{
+public class ViewPL implements View {
 
-    private Scanner scanner = new Scanner (System.in);
+    private Scanner scanner = new Scanner(System.in);
 
-    private void log(String text){
+    private void log(String text) {
         System.out.println(text);
     }
+
     @Override
     public MenuOption showMenuAndGetOption() {
         log("Wybierz opcję: ");
         log("[" + MenuOption.CIRCLE_FIELD.menuIndex + "]" + " Oblicz pole koła");
-
+        log("[" + MenuOption.CHANGE_LANGUAGE.menuIndex + "]" + " Zmień język na angielski");
         log("[" + MenuOption.EXIT.menuIndex + "]" + " Wyjście z programu");
 
         int menuOption;
 
-        try{
+        try {
             menuOption = scanner.nextInt();
-        }catch (InputMismatchException e){
+        } catch (InputMismatchException e) {
             scanner.next();
             log("Wybierz poprawną wartość liczbową z listy opcji");
             return null;
@@ -36,13 +36,22 @@ public class ViewPL implements View{
     @Override
     public Circle promptForCircleParameters() {
         log("Podaj długość promienia koła: ");
-        double r = scanner.nextDouble();
-        Circle circle = new Circle(r);
-        return circle;
+        Double r = null;
+        while (r == null) {
+            try {
+                r = scanner.nextDouble();
+                Circle circle = new Circle(r);
+                return circle;
+            } catch (InputMismatchException e) {
+                log("Wprowadź wartość zmiennoprzecinkową używając przecinka");
+                scanner.next();
+            }
+        }
+        return null;
     }
 
     @Override
     public void showCircleField(double circleField) {
-    log("Pole koła wynosi: " + circleField);
+        log("Pole koła wynosi: " + circleField);
     }
 }
