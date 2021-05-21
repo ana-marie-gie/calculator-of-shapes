931e4a05bf29494b5b154f64ea03422fed1e149c
ania <anagie@icloud.com>
2021-05-21 19:20:28 +0200

    added in ViewPL override of methods: showMenuAndGetOption(), promptForCircleParameters(), showCircleField(double circleField)

--- src/main/java/pl/mvc/view/ViewPL.java
+++ src/main/java/pl/mvc/view/ViewPL.java
@@ -1,4 +1,48 @@
 package pl.mvc.view;
 
+import pl.mvc.model.Circle;
+
+import java.awt.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ViewPL implements View{
+
+    private Scanner scanner = new Scanner (System.in);
+
+    private void log(String text){
+        System.out.println(text);
+    }
+    @Override
+    public MenuOption showMenuAndGetOption() {
+        log("Wybierz opcję: ");
+        log("[" + MenuOption.CIRCLE_FIELD.menuIndex + "]" + " Oblicz pole koła");
+
+        log("[" + MenuOption.EXIT.menuIndex + "]" + " Wyjście z programu");
+
+        int menuOption;
+
+        try{
+            menuOption = scanner.nextInt();
+        }catch (InputMismatchException e){
+            scanner.next();
+            log("Wybierz poprawną wartość liczbową z listy opcji");
+            return null;
+        }
+        return MenuOption.valueOf(menuOption);
+
+    }
+
+    @Override
+    public Circle promptForCircleParameters() {
+        log("Podaj długość promienia koła: ");
+        double r = scanner.nextDouble();
+        Circle circle = new Circle(r);
+        return circle;
+    }
+
+    @Override
+    public void showCircleField(double circleField) {
+    log("Pole koła wynosi: " + circleField);
+    }
 }
